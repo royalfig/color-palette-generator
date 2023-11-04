1415b11a1280fa2745ba09e7d2784de87f4e4417
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2023-11-04 14:25:44 -0400

    start scales

--- src/util/palettes/scales.js
+++ src/util/palettes/scales.js
@@ -1,29 +1,52 @@
-import { hsl, lch } from '../../lib/colorParse.js'
+import { hsl, oklch } from '../../lib/colorParse.js'
 import { colorFactory } from './factory.js'
+import { filterGrayscale, filterContrast, interpolate } from 'culori/fn'
 
-function createBasicScale(baseColor, isLch) {
-  const tints = []
+function createTones(baseColor) {
+  const original = []
+  const keel = []
+  const cinematic = []
+  const languid = []
+  const sharkbite = []
 
-  const color = isLch ? lch(baseColor) : hsl(baseColor)
+  const dark = hsl(baseColor)
+  dark.s = 0.25
+  dark.l = 0.15
+  const light = hsl(baseColor)
+  light.s = 0.15
+  light.l = 0.99
+  const interpolatedColors = interpolate([dark, light])
+
   for (let index = 0; index < 10; index++) {
-    if (isLch) {
-      color.c = 10
-      color.l = index * 10 + 8
-    } else {
-      color.s = 0.1
-      color.l = (index * 10 + 8) / 100
-    }
-    console.log(color)
-    tints.push(colorFactory(color, 'tint', index))
+    const hslBase = hsl(baseColor)
+    const oklchBase = oklch(baseColor)
+
+    oklchBase.c = 10
+    oklchBase.l = index * 10 + 8
+
+    hslBase.s = 0.1
+    hslBase.l = (index * 10 + 8) / 100
+
+    const keelScale = filterGrayscale(0.75)(oklchBase)
+    const lanquidScale = filterContrast(0.5)(oklchBase)
+    // const sharkScale = filterContrast(1.5)(lchBase)
+    original.push(colorFactory(hslBase, 'tint-og', index))
+    keel.push(colorFactory(keelScale, 'tint-cinematic', index))
+    cinematic.push(colorFactory(oklchBase, 'tint-keel', index))
+    languid.push(colorFactory(lanquidScale, 'tint-languid', index))
+    sharkbite.push(colorFactory(interpolatedColors(index / 10), 'tint-sharkbite', index))
   }
-  return tints
+  return { original, keel, cinematic, languid, sharkbite }
 }
 
-export function createTones(baseColor) {
-  const original = createBasicScale(baseColor)
-  const keel = createBasicScale(baseColor, 'lch')
+function createPolychroma(baseColor) {}
 
-  console.log(original, keel)
+function createTones(tintsAndShades) {}
+
+export function create(baseColor) {
+  const scales = createTones(baseColor)
+
+  console.log(scales)
 }
 
-createTones('#ff0000')
+create('#ff0000')
