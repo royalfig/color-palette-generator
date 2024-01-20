87f8423ac302201f337c01e51603a4ef2b03c835
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2024-01-20 11:47:25 -0500

    Refactor button and display styles

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,12 +1,21 @@
-import "./button.css";
+import './button.css'
 
-export default function Button({ handler, active, children }: { handler: () => void; active: boolean; children: React.ReactNode }) {
-  const activeClass = active ? "button active" : "button";
+export default function Button({
+  handler,
+  active,
+  children,
+}: {
+  handler: () => void
+  active: boolean
+  children: React.ReactNode
+}) {
+  const activeClass = active ? 'button active' : 'button'
+
   return (
     <div className="button-container">
       <button className={activeClass} onClick={handler}>
         {children}
       </button>
     </div>
-  );
+  )
 }
--- src/components/palette_selector/PaletteSelector.tsx
+++ src/components/palette_selector/PaletteSelector.tsx
@@ -11,12 +11,15 @@ export default function PaletteSelector({
   palette: string
   setPalette: Function
 }) {
-  console.log(Object.entries(palettes))
   return (
     <div className="palette-selector-container">
       {Object.entries(palettes).map(([key, value]) => {
         return (
-          <button key={key} className="palette-selector">
+          <button
+            key={key}
+            onClick={() => setPalette(key)}
+            className={palette === key ? `active palette-selector` : `palette-selector`}
+          >
             {['tones', 'tints', 'polychromia'].includes(key) ? (
               <Circle colors={value} type="circle" size="small" />
             ) : (
@@ -26,26 +29,6 @@ export default function PaletteSelector({
           </button>
         )
       })}
-
-      {/* {palettes.map((colors, idx) => {
-        return (
-          <button key={idx} onClick={setPalette}>
-            <Circle
-              colors={colors}
-              type={
-                colors.name === 'tones' ||
-                colors.name === 'tints and shades' ||
-                colors.name === 'polychroma' ||
-                colors.name === 'ombre'
-                  ? 'circle'
-                  : 'default'
-              }
-              size="small"
-            />
-            <p>{colors.name}</p>
-          </button>
-        )
-      })} */}
     </div>
   )
 }
