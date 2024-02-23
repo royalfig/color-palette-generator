c5c224ea6507c53854d07e5401972f7f2a3714b3
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2024-02-23 10:48:52 -0500

    Delete unused CSS and component files

--- src/components/loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import "./loader.css"
-export default function Loader({children, loading}) {
-    const className = loading ? "loader loading" : "loader";
-    return (
-        <div className={className}>
-            {children}
-        </div>
-    );
-}
